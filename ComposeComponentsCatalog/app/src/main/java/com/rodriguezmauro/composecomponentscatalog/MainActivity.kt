98b9e77716138503d69c2abf5bfa295d375cc814
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2024-09-04 21:56:28 -0300

    added images and progress

--- ComposeComponentsCatalog/app/src/main/java/com/rodriguezmauro/composecomponentscatalog/MainActivity.kt
+++ ComposeComponentsCatalog/app/src/main/java/com/rodriguezmauro/composecomponentscatalog/MainActivity.kt
@@ -4,9 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
@@ -19,9 +35,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -38,13 +57,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeComponentsCatalogTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(Modifier.padding(innerPadding)) {
+                    Column(
+                        Modifier
+                            .padding(innerPadding)
+                            .verticalScroll(rememberScrollState())) {
                         var myText by rememberSaveable {
                             mutableStateOf("")
                         }
                         MyTextField("Ejemplo de state hosting") {
                             myText = it
                         }
+
+                        MyImage()
+
+                        MyImageAdvanced()
+
+                        MyIcon()
+
+                        MyProgress()
+
+                        MyProgressAdvance()
                     }
                 }
             }
@@ -61,8 +93,92 @@ fun GreetingPreview() {
 }
 
 @Composable
+fun MyProgressAdvance() {
+    var progressStatus by rememberSaveable {
+        mutableStateOf(0f)
+    }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LinearProgressIndicator(progress = { progressStatus })
+        Row(Modifier.fillMaxWidth()) {
+            Button(onClick = { progressStatus += 0.1f }) {
+                Text(text = "Incrementar")
+            }
+            Button(onClick = { progressStatus -= 0.1f}) {
+                Text(text = "Reducir")
+            }
+        }
+    }
+}
+
+@Composable
+fun MyProgress() {
+    var showLoading by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    Column(
+        Modifier
+            .padding(24.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (showLoading) {
+            CircularProgressIndicator(color = Color.Red, strokeWidth = 10.dp)
+            LinearProgressIndicator(
+                modifier = Modifier.padding(top = 32.dp),
+                color = Color.Red,
+                trackColor = Color.Green
+            )
+        }
+        
+        Button(onClick = {
+            showLoading = !showLoading
+        }) {
+            Text(text = "Cargar perfil")
+        }
+    }
+
+}
+
+
+@Composable
+fun MyIcon() {
+    Icon(
+        imageVector = Icons.Rounded.Star,
+        contentDescription = "Icono",
+        tint = Color.Red
+    )
+}
+
+@Composable
+fun MyImageAdvanced() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_background),
+        contentDescription = "ejemplo",
+        modifier = Modifier
+            .width(150.dp)
+            .height(50.dp)
+            .clip(CircleShape)
+            .border(5.dp, Color.Red, CircleShape)
+    )
+}
+
+@Composable
+fun MyImage() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_background),
+        contentDescription = "content descrip",
+        alpha = 0.5f
+    )
+}
+
+@Composable
 fun MyTextFieldOutlined() {
-    Column(Modifier.fillMaxSize()) {
+    Column {
         var myText by rememberSaveable {
             mutableStateOf("")
         }
@@ -89,7 +205,7 @@ fun MyTextFieldOutlined() {
 
 @Composable
 fun MyTextFieldAdvanced() {
-    Column(Modifier.fillMaxSize()) {
+    Column {
         var myText by rememberSaveable {
             mutableStateOf("")
         }
@@ -104,14 +220,14 @@ fun MyTextFieldAdvanced() {
 
 @Composable
 fun MyTextField(name: String, onValueChanged:(String) -> Unit ) {
-    Column(Modifier.fillMaxSize()) {
+    Column {
         TextField(value = name, onValueChange = { onValueChanged(it) })
     }
 }
 
 @Composable
 fun MyText() {
-    Column(Modifier.fillMaxSize()) {
+    Column {
         Text(text = "Esto es un ejemplo")
         Text(text = "Esto es un ejemplo", color = Color.Blue)
         Text(text = "Esto es un ejemplo", fontWeight = FontWeight.Medium)
