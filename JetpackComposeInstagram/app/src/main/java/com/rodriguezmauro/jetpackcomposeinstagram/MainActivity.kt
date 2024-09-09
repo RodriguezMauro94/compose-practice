16ae55ebdf31c27510f61a66f9b869ae99104812
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2024-09-09 20:37:02 -0300

    added JetpackComposeInstagram

---
+++ JetpackComposeInstagram/app/src/main/java/com/rodriguezmauro/jetpackcomposeinstagram/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.rodriguezmauro.jetpackcomposeinstagram
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rodriguezmauro.jetpackcomposeinstagram.ui.theme.JetpackComposeInstagramTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackComposeInstagramTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        LoginScreen()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeInstagramTheme {
+        Greeting("Android")
+    }
+}
