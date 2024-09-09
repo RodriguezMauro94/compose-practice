104334db7914c58f5b9f8d0fa69b33000c791eb9
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2024-09-08 23:20:41 -0300

    Added cards, badgedbox, divider and dropdownmenu

--- ComposeComponentsCatalog/app/src/main/java/com/rodriguezmauro/composecomponentscatalog/MainActivity.kt
+++ ComposeComponentsCatalog/app/src/main/java/com/rodriguezmauro/composecomponentscatalog/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,8 +21,13 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
@@ -33,12 +40,17 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.TriStateCheckbox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -113,9 +125,104 @@ class MainActivity : ComponentActivity() {
                         MyRadioButtonList(name = stateRadioButtonsSelected) {
                             stateRadioButtonsSelected = it
                         }
+
+                        MyCard()
+
+                        MyBadgedBox()
+
+                        MyDivider()
+
+                        MyDropDownMenu()
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun MyDropDownMenu() {
+    var selectedText by remember {
+        mutableStateOf("")
+    }
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    val desserts = listOf("Helado", "Chocolate", "Cafe", "Frutas")
+
+    Column(
+        modifier = Modifier.padding(20.dp)
+    ) {
+        OutlinedTextField(
+            value = selectedText,
+            onValueChange = {
+                selectedText = it
+            },
+            enabled = false,
+            readOnly = true,
+            modifier = Modifier
+                .clickable {
+                    expanded = true
+                }
+                .fillMaxWidth()
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = {
+                expanded = false
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            desserts.forEach{
+                DropdownMenuItem(text = { Text(text = it) }, onClick = {
+                    selectedText = it
+                    expanded = false
+                })
+            }
+        }
+    }
+}
+
+@Composable
+fun MyDivider() {
+    HorizontalDivider(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp),
+        color = Color.Red
+    )
+}
+
+@Composable
+fun MyBadgedBox() {
+    BadgedBox(
+        modifier = Modifier.padding(16.dp),
+        badge = {
+            Badge(content = { Text(text = "100") },
+                containerColor = Color.Blue, contentColor = Color.White)
+            }
+        ) {
+        Icon(imageVector = Icons.Default.Star, contentDescription = "Star")
+    }
+}
+
+
+@Composable
+fun MyCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        elevation = CardDefaults.cardElevation(12.dp),
+        shape = MaterialTheme.shapes.small,
+        colors = CardDefaults.cardColors(containerColor = Color.Red, contentColor = Color.Green),
+        border = BorderStroke(5.dp, Color.Magenta)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = "Ejemplo 1")
+            Text(text = "Ejemplo 2")
+            Text(text = "Ejemplo 3")
         }
     }
 }
