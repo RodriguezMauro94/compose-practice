fdb3b16dde0f8e884aa85ec1e313dea1107157b4
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2024-09-09 21:02:46 -0300

    login screen part 1

--- JetpackComposeInstagram/app/src/main/java/com/rodriguezmauro/jetpackcomposeinstagram/LoginScreen.kt
+++ JetpackComposeInstagram/app/src/main/java/com/rodriguezmauro/jetpackcomposeinstagram/LoginScreen.kt
@@ -1,25 +1,43 @@
 package com.rodriguezmauro.jetpackcomposeinstagram
 
 import android.app.Activity
+import android.health.connect.datatypes.BloodPressureRecord.BodyPosition
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoginScreen() {
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .padding(8.dp)) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
         Header(Modifier.align(Alignment.TopEnd))
+        Body(modifier = Modifier.align(Alignment.Center))
     }
 }
 
@@ -32,5 +50,63 @@ fun Header(modifier: Modifier) {
         modifier = modifier.clickable {
             activity.finish()
         }
+    )
+}
+
+@Composable
+fun Body(modifier: Modifier) {
+    var email by rememberSaveable {
+        mutableStateOf("")
+    }
+    var password by rememberSaveable {
+        mutableStateOf("")
+    }
+    Column(
+        modifier = modifier
+    ) {
+        ImageLogo()
+        Spacer(modifier = Modifier.size(16.dp))
+        Email(email) {
+            email = it
+        }
+        Spacer(modifier = Modifier.size(4.dp))
+        Password(password) {
+            password = it
+        }
+        Spacer(modifier = Modifier.size(8.dp))
+        ForgotPassword(modifier = Modifier.align(Alignment.End))
+    }
+}
+
+@Composable
+fun ForgotPassword(modifier: Modifier) {
+    Text(
+        text = "Forgot password",
+        modifier = modifier,
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color(0xff4EA8E9)
+    )
+}
+
+@Composable
+fun Password(password: String, onTextChanged: (String) -> Unit) {
+    TextField(value = password, onValueChange = {
+        onTextChanged(it)
+    })
+}
+
+@Composable
+fun Email(email: String, onTextChanged: (String) -> Unit) {
+    TextField(value = email, onValueChange = {
+        onTextChanged(it)
+    })
+}
+
+@Composable
+fun ImageLogo() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_insta_logo),
+        contentDescription = "Instagram"
     )
 }
