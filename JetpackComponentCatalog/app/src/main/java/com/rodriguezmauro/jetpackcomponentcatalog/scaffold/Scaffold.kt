ea6262f5638403149c6802e40b69bbfcc29820e7
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2024-11-06 23:08:01 -0300

    added bottom bar

--- JetpackComponentCatalog/app/src/main/java/com/rodriguezmauro/jetpackcomponentcatalog/scaffold/Scaffold.kt
+++ JetpackComponentCatalog/app/src/main/java/com/rodriguezmauro/jetpackcomponentcatalog/scaffold/Scaffold.kt
@@ -9,11 +9,17 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarDuration
@@ -23,8 +29,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -56,7 +65,8 @@ fun ScaffoldExample() {
                     contentColor = Color.Blue
                 )
             })
-        }
+        },
+        bottomBar = { MyBottomNavigation() }
     ) { padding ->
         Box(
             Modifier
@@ -94,4 +104,75 @@ fun MyTopAppBar(onClick: (String) -> Unit) {
                 Icon(imageVector = Icons.Filled.Warning, contentDescription = "Warning")
             }
         })
+}
+
+@Composable
+fun MyBottomNavigation() {
+    var index by remember {
+        mutableIntStateOf(0)
+    }
+    NavigationBar {
+        NavigationBarItem(
+            selected = index == 0,
+            onClick = { index = 0 },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = "Home"
+                )
+            },
+            colors = NavigationBarItemDefaults.colors(
+                indicatorColor = Color.Red,
+                selectedTextColor = Color.Gray,
+                unselectedTextColor = Color.Gray,
+                selectedIconColor = Color.White,
+                unselectedIconColor = Color.Gray
+            ),
+            label = {
+                Text(text = "Home")
+            }
+        )
+
+        NavigationBarItem(
+            selected = index == 1,
+            onClick = { index = 1 },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = "Favoritos"
+                )
+            },
+            colors = NavigationBarItemDefaults.colors(
+                indicatorColor = Color.Red,
+                selectedTextColor = Color.Gray,
+                unselectedTextColor = Color.Gray,
+                selectedIconColor = Color.White,
+                unselectedIconColor = Color.Gray
+            ),
+            label = {
+                Text(text = "Favs")
+            }
+        )
+
+        NavigationBarItem(
+            selected = index == 2,
+            onClick = { index = 2 },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "Perfil"
+                )
+            },
+            colors = NavigationBarItemDefaults.colors(
+                indicatorColor = Color.Red,
+                selectedTextColor = Color.Gray,
+                unselectedTextColor = Color.Gray,
+                selectedIconColor = Color.White,
+                unselectedIconColor = Color.Gray
+            ),
+            label = {
+                Text(text = "Perfil")
+            }
+        )
+    }
 }
